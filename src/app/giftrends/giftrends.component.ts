558e9e7b829d0dae4ad3ec7f2aea78d3c2839667
Dindihub <sadindi03@gmail.com>
2022-04-14 15:42:47 +0300

    Ft-Add component

--- src/app/giftrends/giftrends.component.ts
+++ src/app/giftrends/giftrends.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { GifserviceService } from '../gifservice.service';
 
 @Component({
   selector: 'app-giftrends',
   templateUrl: './giftrends.component.html',
-  styleUrls: ['./giftrends.component.css']
+  styleUrls: ['./giftrends.component.css'],
+  providers:[GifserviceService]
 })
 export class GiftrendsComponent implements OnInit {
 
